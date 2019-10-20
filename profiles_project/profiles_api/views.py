d3e222785e492c78cf4d8a9de653cea7d70d02b0
saahilmehra <saahilmehra1998@gmail.com>
2019-10-20 15:42:20 +0530

    Added profile listing and creating feature.

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from . import models
+from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from . import serializers
+from rest_framework import status
 
 # Create your views here.
+class UserProfileViewSet(viewsets.ModelViewSet):
+    serializer_class=serializers.UserProfileSerializer
+    queryset=models.UserProfile.objects.all()
