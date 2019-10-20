e39e80067ee78df465111fb7e44f5cd1f375ea51
saahilmehra <saahilmehra1998@gmail.com>
2019-10-20 20:24:28 +0530

    Added login API

--- profiles_project/profiles_api/urls.py
+++ profiles_project/profiles_api/urls.py
@@ -4,6 +4,7 @@ from rest_framework.routers import DefaultRouter
 
 router=DefaultRouter()
 router.register('profile',views.UserProfileViewSet)
+router.register('login',views.LoginViewSet,base_name='login')
 
 
 urlpatterns = [
